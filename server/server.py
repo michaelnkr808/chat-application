945d03c1bc04e5eb6916cd2c96488f1428a17550
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-11 20:56:27 -0700

    Add multiple client functionality

--- server/server.py
+++ server/server.py
@@ -3,22 +3,42 @@ import threading
 
 HOST = ''
 PORT = 9999
+clients = []
+usernames = {}
 
-def handleConnections(conn, addr):
+def handle_connections(conn, addr):
     with conn: 
         print("Connected by:", addr)
         while True:
             data = conn.recv(1024)
-            if not data: break
-            conn.sendall(data)
+            if not data:
+                clients.remove(conn)
+                break
+            for client in clients:
+                try:
+                    client.sendall(data)
+                except Exception as e:
+                    print(f"Error sending to client {e}")
+                    client.close()
+                    clients.remove(client)
+def retrieve_username(conn):
+    username = input("Enter your usernmae: ")
+    with conn:
+        conn.send(username)
+        
+        
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     serversocket.bind((HOST, PORT))
     serversocket.listen(5)
     while True: 
         conn, addr = serversocket.accept()
-        connectThread = threading.Thread(target=handleConnections, args=(conn, addr))
+        connectThread = threading.Thread(target=handle_connections, args=(conn, addr))
         connectThread.start()
+        clients.append(conn)
+        
+
 
     
 
