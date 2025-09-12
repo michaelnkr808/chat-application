4256b8040a58ca19c6304fa906bda3bedd1fe19b
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-11 18:16:32 -0700

    Add basic threading to handle multiple clients

--- server/server.py
+++ server/server.py
@@ -1,16 +1,26 @@
 import socket
+import threading
 
 HOST = ''
 PORT = 9999
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
-    serversocket.bind((HOST, PORT))
-    serversocket.listen(5)
-    conn, addr = serversocket.accept()
+def handleConnections(conn, addr):
     with conn: 
         print("Connected by:", addr)
         while True:
             data = conn.recv(1024)
             if not data: break
             conn.sendall(data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
+    serversocket.bind((HOST, PORT))
+    serversocket.listen(5)
+    while True: 
+        conn, addr = serversocket.accept()
+        connectThread = threading.Thread(target=handleConnections, args=(conn, addr))
+        connectThread.start()
+
+    
+
+
     
