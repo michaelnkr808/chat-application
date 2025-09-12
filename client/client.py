0a7b069ca4fa58ad010cba72172b138dd87a43e7
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-11 23:46:51 -0700

    Add client messaging and recieving

--- client/client.py
+++ client/client.py
@@ -1,9 +1,32 @@
 import socket
+import threading
 
 HOST = ''
 PORT = 9999
+
+def send_message(s):
+    while True:
+        message = input("Enter a message ('quit') to close): ")
+        s.sendall(message.encode())
+        if message == "quit":
+            try:
+                s.close()
+            except Exception:
+                print("Client excited successfully") 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     s.sendall(b'Hello, world')
-    data = s.recv(1024)
-print('Received', repr(data))
+    enter_username = s.recv(1024).decode()
+    print(enter_username)
+    send_thread = threading.Thread(target=send_message, args=(s,))
+    send_thread.start()
+    while True:
+        try:
+            print(s.recv(1024).decode())
+        except Exception:
+            print("Client exited successfully")
+            break
+        
+       
+
