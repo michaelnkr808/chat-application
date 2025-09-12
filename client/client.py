fc17773db073946cea865aa8873a31768cd7561a
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-12 00:48:19 -0700

    Fix brokenpipe errors

--- client/client.py
+++ client/client.py
@@ -1,32 +1,49 @@
 import socket
 import threading
 
-HOST = ''
+HOST = 'localhost'
 PORT = 9999
+PROMPT = "Enter a message ('quit' to close): "
 
 def send_message(s):
     while True:
-        message = input("Enter a message ('quit') to close): ")
-        s.sendall(message.encode())
-        if message == "quit":
-            try:
-                s.close()
-            except Exception:
-                print("Client excited successfully") 
+        try:
+            message = input(PROMPT)
+            s.sendall(message.encode())
+            if message == "quit":
+                try:
+                    s.shutdown(socket.SHUT_RDWR)
+                except Exception:
+                    pass
+                try:
+                    s.close()
+                except Exception:
+                    pass
+                break
+        except(BrokenPipeError, OSError):
+            print("Client exited successfully")
+            break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
     enter_username = s.recv(1024).decode()
-    print(enter_username)
-    send_thread = threading.Thread(target=send_message, args=(s,))
-    send_thread.start()
-    while True:
-        try:
-            print(s.recv(1024).decode())
-        except Exception:
-            print("Client exited successfully")
-            break
+    username = input(enter_username)
+    if(username):
+        s.send(username.encode())
+        send_thread = threading.Thread(target=send_message, args=(s,))
+        send_thread.daemon = True
+        send_thread.start()
+        while True:
+            try:
+                data = s.recv(1024)
+                if not data:
+                    print("Client exited successfully")
+                    break
+                print(data.decode())
+                print(PROMPT, end='', flush=True)
+            except Exception:
+                print("Client exited successfully")
+                break
         
        
 
--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 
-HOST = ''
+HOST = '0.0.0.0'
 PORT = 9999
 clients = []
 usernames = {}
@@ -13,18 +13,21 @@ def handle_connections(conn, addr):
         username = conn.recv(1024).decode().strip()
         usernames[conn] = username
         while True:
-            data = conn.recv(1024)
-            if not data:
-                clients.remove(conn)
+            try:
+                data = conn.recv(1024)
+                if not data:
+                    clients.remove(conn)
+                    break
+            except OSError:
                 break
             for client in clients:
                 try:
-                    client.sendall(f"[{username}]: {data.decode()}")
+                    client.sendall(f"[{username}]: {data.decode()}".encode())
                 except Exception as e:
                     print(f"Error sending to client {e}")
                     client.close()
                     clients.remove(client)
-                    del usernames[conn]
+                    del usernames[client] 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     serversocket.bind((HOST, PORT))
@@ -32,8 +35,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     while True: 
         conn, addr = serversocket.accept()
         connectThread = threading.Thread(target=handle_connections, args=(conn, addr))
-        connectThread.start()
         clients.append(conn)
+        connectThread.start()
         
 
 
