dd0f30eb65c905b158a9a9bbb5d43853a2268bea
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2025-09-13 23:53:14 -0700

    Add username authentication flag to prevent chat if invalid username

--- client/client.py
+++ client/client.py
@@ -4,6 +4,7 @@ import threading
 HOST = 'localhost'
 PORT = 9999
 PROMPT = "Enter a message ('quit' to close): "
+authenticated_username = False
 
 def send_message(s):
     while True:
@@ -26,14 +27,24 @@ def send_message(s):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    enter_username = s.recv(1024).decode()
-    username = input(enter_username)
-    if(username):
-        s.send(username.encode())
+    while True:
+        enter_username = s.recv(1024).decode().strip()
+        username = input(enter_username)
+        if(username):
+            s.send(username.encode())
+            enter_username = s.recv(1024).decode().strip()
+            if(enter_username.startswith("Welcome")):
+                authenticated_username = True
+                break
+            else:
+                print(enter_username)
+    #check for valid username
+    if(authenticated_username):
         send_thread = threading.Thread(target=send_message, args=(s,))
         send_thread.daemon = True
         send_thread.start()
-        while True:
+    while True:
+        if(authenticated_username):
             try:
                 data = s.recv(1024)
                 if not data:
@@ -44,6 +55,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             except Exception:
                 print("Client exited successfully")
                 break
-        
+            
        
 
